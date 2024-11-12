08a39694a021a9e7b701661b37974a6be6670dbc
Anna Bogoslavskaya <agbogoslavskaya@MSK-LGYH7LWV42.local>
2024-11-12 07:05:55 +0300

    deleted unimportant files

--- tmp.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import make_classification
-import pandas as pd
-
-X, y = make_classification(random_state=42)
-df = pd.DataFrame(X)
-df['target'] = y
-
-# df.to_csv('data.csv', index=False)
