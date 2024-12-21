b9e6d6c3e01e98b77e53016f2a8776a124ad99f2
Anna Bogoslavskaya <agbogoslavskaya@avito.ru>
2024-12-22 01:02:29 +0300

    add clear ml

--- app/main.py
+++ app/main.py
@@ -21,6 +21,8 @@ from mlops_pipeline.minio_utils import (
     upload_model_to_minio,
     upload_dataset_to_minio_and_track_with_dvc,
 )
+from clearml import Task
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,72 +30,86 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 setts = Settings()
 
-
 class TrainRequestModel(BaseModel):
-    ml_model_type: str
-    ml_model_params: Dict[str, Any]
-    data: str  #
-    target_column: str
+    ml_model_type: str  
+    ml_model_params: Dict[str, Any]  
+    data: str  # 
+    target_column: str  
 
+import os
 
-@app.post("/train/")
+@app.post('/train/')
 async def train_model(request: TrainRequestModel):
     try:
+        task = Task.init(
+            project_name='MLops Project',
+            task_name=f"Train {request.ml_model_type}",
+            task_type=Task.TaskTypes.training,
+        )
+
         model_params = ModelParams(
-            ml_model_type=request.ml_model_type, ml_model_params=request.ml_model_params
+            ml_model_type=request.ml_model_type, 
+            ml_model_params=request.ml_model_params
         )
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=f"Validation error: {e}")
 
-    model = Model(model_params)
-
+    model = Model(model_params)  
+    
     try:
-        data_df = pd.read_csv(StringIO(request.data))
+        data_df = pd.read_csv(StringIO(request.data))  
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to read CSV: {str(e)}")
 
     try:
         x_train, y_train, x_test, y_test = parse_data(
-            data_df, target=request.target_column, split=True
+            data_df, 
+            target=request.target_column,  
+            split=True  
         )
     except Exception as e:
-        raise HTTPException(
-            status_code=400, detail=f"Data parsing and splitting failed: {str(e)}"
-        )
+        raise HTTPException(status_code=400, detail=f"Data parsing and splitting failed: {str(e)}")
 
-    datasets_dir = "datasets"
+    datasets_dir = 'datasets'
     if not os.path.exists(datasets_dir):
-        os.makedirs(datasets_dir)
+        os.makedirs(datasets_dir)  
 
-    local_dataset_path = os.path.join(datasets_dir, "training_data.csv")
+    local_dataset_path = os.path.join(datasets_dir, 'training_data.csv')
     data_df.to_csv(local_dataset_path, index=False)
-
+    
     bucket_name = "models-bucket"
     dataset_object_name = f"{model_params.ml_model_type}_training_data.csv"
-    upload_dataset_to_minio_and_track_with_dvc(
-        local_dataset_path, bucket_name, dataset_object_name
-    )
+    upload_dataset_to_minio_and_track_with_dvc(local_dataset_path, bucket_name, dataset_object_name)
 
     model.train(x_train, y_train)
 
-    local_model_path = model.save("models")
-
+    local_model_path = model.save('models') 
+    
     object_name = f"{model_params.ml_model_type}_model.pkl"
     upload_model_to_minio(local_model_path, bucket_name, object_name)
+
+    task.upload_artifact(name="model", artifact_object=local_model_path)
+    
+    task.close()
+
+    project = task.project
+    task_url = f"https://app.clear.ml/projects/{project}/experiments/{task.id}/output/log"
 
     return {
         "status": "success",
         "message": f"Model {model_params.ml_model_type} trained, saved locally, dataset and model uploaded to Minio!",
+        "task_id": task.id,  
+        "task_url": task_url  
     }
 
 
 class PredictRequestModel(BaseModel):
-    model_type: str
-    data: str
-    target_column: str
+    model_type: str  
+    data: str  
+    target_column: str  
 
 
-@app.post("/predict/")
+@app.post('/predict/')
 async def predict(request: PredictRequestModel) -> Dict[str, Any]:
     """
     Returns predictions from a trained model along with accuracy and classification report.
@@ -101,76 +117,92 @@ async def predict(request: PredictRequestModel) -> Dict[str, Any]:
     model = Model()
     model.load(setts.MODEL_DIR, model_type=request.model_type)
     logger.info(f"Model {request.model_type} loaded successfully.")
+    
+    task = Task.init(
+        project_name='MLops Project',
+        task_name=f"Prediction with {request.model_type}",
+        task_type=Task.TaskTypes.inference,
+        reuse_last_task_id=False
+    )
 
+    task_logger = task.get_logger()  
+    
     try:
+        task_logger.report_text(f"Model {request.model_type} loaded successfully.")
+        task_logger.report_text("Task created successfully.")
+        
         data_df = pd.read_csv(StringIO(request.data))
-        logger.info("Data loaded successfully.")
-
-        x_train, y_train, x_test, y_test = parse_data(
-            data_df, target=request.target_column, split=True
-        )
-        logger.info(
-            f"Data split successfully: x_test shape: {x_test.shape}, y_test shape: {y_test.shape}"
-        )
-
+        task_logger.report_text("Data loaded successfully.")
+        
+        x_train, y_train, x_test, y_test = parse_data(data_df, target=request.target_column, split=True)
+        task_logger.report_text(f"Data split successfully: x_test shape: {x_test.shape}, y_test shape: {y_test.shape}")
+        
         predictions = model.predict(x_test)
-
+        
         if isinstance(predictions, np.ndarray):
             predictions = predictions.flatten()
-
+        
         predictions_list = predictions.tolist()
         accuracy = accuracy_score(y_test, predictions)
-        # logger.info(f"Accuracy on test set: {accuracy:.4f}")
-
+        
         class_report = None
         if len(np.unique(y_test)) == 2:
             class_report = classification_report(y_test, predictions)
-            # logger.info(f"Classification Report:\n{class_report}")
-
-        # CF
+        
         cm = confusion_matrix(y_test, predictions)
         cm_list = cm.tolist()
-
-        # ROC-AUC
+        
         fpr, tpr, thresholds = roc_curve(y_test, predictions)
         roc_auc = roc_auc_score(y_test, predictions)
 
-        return {
-            "predictions": predictions_list,
-            "y_true": y_test.tolist(),
-            "accuracy": accuracy,
-            "classification_report": class_report,
-            "confusion_matrix": cm_list,
-            "fpr": fpr.tolist(),
-            "tpr": tpr.tolist(),
-            "roc_auc": roc_auc,
-        }
+        task.upload_artifact(name="confusion_matrix", artifact_object=cm_list)
+        task.upload_artifact(name="classification_report", artifact_object=class_report)
 
+        task_logger.report_text(f"Prediction completed: accuracy = {accuracy:.4f}, ROC AUC = {roc_auc:.4f}")
+        
+        task.close()
+
+        project = task.project
+        task_url = f"https://app.clear.ml/projects/{project}/experiments/{task.id}/output/log"
+
+        return {
+            'predictions': predictions_list,
+            'y_true': y_test.tolist(),
+            'accuracy': accuracy,
+            'classification_report': class_report,
+            'confusion_matrix': cm_list,
+            'fpr': fpr.tolist(),
+            'tpr': tpr.tolist(),
+            'roc_auc': roc_auc,
+            'task_id': task.id,  
+            'task_url': task_url  
+        }
+    
     except Exception as e:
+        task_logger.report_text(f"Error in prediction: {str(e)}")
         logger.error(f"Error in prediction: {str(e)}")
         raise HTTPException(status_code=400, detail=f"Prediction error: {str(e)}")
 
 
-@app.get("/remove/")
+@app.get('/remove/')
 def remove_trained_model(model_type: ModelClass):
     """
     Deletes a trained model.
     """
-    model_file = setts.MODEL_DIR / f"{model_type}_model.pkl"
+    model_file = setts.MODEL_DIR / f'{model_type}_model.pkl'
     if model_file.is_file():
         model_file.unlink()
 
-
-@app.get("/info")
+@app.get('/info')
 def get_model_list() -> Dict[str, List[str]]:
     """
     Returns a list of models that can be trained
     """
     model_list = [el for el in ModelClass.__members__.values()]
-    return {"models_available": model_list}
+    return {'models_available': model_list}
 
 
-@app.get("/status")
+@app.get('/status')
 def get_status() -> Dict[str, ServiceStatus]:
     """
     Returns the current status of the service
--- app/mlops_pipeline/streamlit.py
+++ app/mlops_pipeline/streamlit.py
@@ -2,18 +2,26 @@ import numpy as np
 import pandas as pd
 import requests
 import streamlit as st
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, roc_auc_score
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+from clearml import Task
 
 FASTAPI_URL = "http://uvicorn:8000"
 
+def fetch_clearml_task(task_id):
+    try:
+        task = Task.get_task(task_id=task_id)
+        return task
+    except Exception as e:
+        st.error(f"Error fetching ClearML task: {str(e)}")
+        return None
 
 def fetch_model_list():
     try:
         response = requests.get(f"{FASTAPI_URL}/info")
         if response.status_code == 200:
-            model_list = response.json()["models_available"]
+            model_list = response.json()['models_available']
             return model_list
         else:
             st.error(f"Error fetching model list: {response.text}")
@@ -22,48 +30,44 @@ def fetch_model_list():
         st.error(f"Request Error: {str(e)}")
         return []
 
-
 def get_available_model():
     available_models = fetch_model_list()
     if not available_models:
-        st.warning(
-            "No models available. Please ensure your FastAPI server is running and accessible."
-        )
+        st.warning("No models available. Please ensure your FastAPI server is running and accessible.")
     return available_models
-
 
 def predict_model(model_choice, data_str, target_column):
     try:
         payload = {
             "model_type": model_choice,
             "data": data_str,
-            "target_column": target_column,
+            "target_column": target_column
         }
         response = requests.post(f"{FASTAPI_URL}/predict/", json=payload)
-
+        
         if response.status_code == 200:
             result = response.json()
-            predictions = np.array(result["predictions"])
-            y_true = np.array(result["y_true"])
-            accuracy = result["accuracy"]
-            class_report = result["classification_report"]
-            cm = np.array(result["confusion_matrix"])
-            fpr = np.array(result["fpr"])
-            tpr = np.array(result["tpr"])
-            roc_auc = result["roc_auc"]
-            return predictions, y_true, accuracy, class_report, cm, fpr, tpr, roc_auc
+            predictions = np.array(result['predictions'])
+            y_true = np.array(result['y_true'])
+            accuracy = result['accuracy']
+            class_report = result['classification_report']
+            cm = np.array(result['confusion_matrix'])
+            fpr = np.array(result['fpr'])
+            tpr = np.array(result['tpr'])
+            roc_auc = result['roc_auc']
+            task_id = result['task_id'] 
+            task_url = result['task_url']  
+            return predictions, y_true, accuracy, class_report, cm, fpr, tpr, roc_auc, task_id, task_url
         else:
             st.error(f"Error during prediction: {response.text}")
-            return None, None, None, None, None, None, None, None
+            return None, None, None, None, None, None, None, None, None, None
     except requests.exceptions.RequestException as e:
         st.error(f"Request Error: {str(e)}")
-        return None, None, None, None, None, None, None, None
+        return None, None, None, None, None, None, None, None, None, None
 
 
 def streamlit_logic():
-    uploaded_data = st.file_uploader(
-        "Upload your CSV file", type=["csv"], key="data_uploader"
-    )
+    uploaded_data = st.file_uploader("Upload your CSV file", type=["csv"], key="data_uploader")
 
     if uploaded_data is not None:
         data = pd.read_csv(uploaded_data)
@@ -76,7 +80,7 @@ def streamlit_logic():
         if model_choice is not None:
             model_params = {
                 "ml_model_type": model_choice,
-                "ml_model_params": {},
+                "ml_model_params": {}, 
             }
 
             data_str = uploaded_data.getvalue().decode("utf-8")
@@ -85,77 +89,61 @@ def streamlit_logic():
                 payload = {
                     "ml_model_type": model_choice,
                     "ml_model_params": model_params,
-                    "data": data_str,
-                    "target_column": target_column,
+                    "data": data_str,  
+                    "target_column": target_column  
                 }
 
-                with st.spinner(f"Training the {model_choice} model..."):
+                with st.spinner(f'Training the {model_choice} model...'):
                     try:
                         response = requests.post(f"{FASTAPI_URL}/train/", json=payload)
                         if response.status_code == 200:
                             success_message = response.json().get("message")
                             st.success(success_message)
+                            
+                            task_id = response.json().get("task_id") 
+                            task_url = response.json().get("task_url")
+                            
+                            if task_id:
+                                st.write(f"Training Task ID: {task_id}")
+                                st.write(f" Training results URL on Clear Ml: {task_url}")
                         else:
                             st.error(f"Error training model: {response.text}")
                     except requests.exceptions.RequestException as e:
                         st.error(f"Request Error: {str(e)}")
-
+            
             if st.button("Make Prediction"):
-                with st.spinner(f"Making predictions with {model_choice}..."):
-                    (
-                        predictions,
-                        y_true,
-                        accuracy,
-                        class_report,
-                        cm,
-                        fpr,
-                        tpr,
-                        roc_auc,
-                    ) = predict_model(model_choice, data_str, target_column)
-
+                with st.spinner(f'Making predictions with {model_choice}...'):
+                    predictions, y_true, accuracy, class_report, cm, fpr, tpr, roc_auc, task_id, task_url = predict_model(model_choice, data_str, target_column)
+                    
                     if predictions is not None:
-                        # st.write("Predictions:")
-                        # st.write(predictions)
 
-                        if target_column in data.columns:
-                            # st.write(f"Accuracy: {accuracy:.4f}")
-                            # st.write("Classification Report:")
-                            # st.text(class_report)
+                        if task_id and task_url:
+                            st.write(f"Prediction Task ID: {task_id}")
+                            st.write(f"Inference results URL on Clear ML: [Task URL]({task_url})")
 
-                            # CF
-                            st.subheader("Confusion Matrix")
-                            fig, ax = plt.subplots()
-                            sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=ax)
-                            plt.xlabel("Predicted")
-                            plt.ylabel("True")
-                            st.pyplot(fig)
+                        st.subheader("Confusion Matrix")
+                        fig, ax = plt.subplots()
+                        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=ax)
+                        plt.xlabel('Predicted')
+                        plt.ylabel('True')
+                        st.pyplot(fig)
 
-                            # ROC-AUC
-                            st.subheader("ROC Curve")
-                            fig_roc, ax_roc = plt.subplots()
-                            ax_roc.plot(
-                                fpr,
-                                tpr,
-                                color="darkorange",
-                                lw=2,
-                                label=f"ROC curve (area = {roc_auc:.2f})",
-                            )
-                            ax_roc.plot(
-                                [0, 1], [0, 1], color="navy", lw=2, linestyle="--"
-                            )
-                            ax_roc.set_xlim([0.0, 1.0])
-                            ax_roc.set_ylim([0.0, 1.05])
-                            ax_roc.set_xlabel("False Positive Rate")
-                            ax_roc.set_ylabel("True Positive Rate")
-                            ax_roc.set_title("Receiver Operating Characteristic (ROC)")
-                            ax_roc.legend(loc="lower right")
-                            st.pyplot(fig_roc)
-
-                            st.subheader("Classification Report")
-                            st.text(class_report)
+                        st.subheader("ROC Curve")
+                        fig_roc, ax_roc = plt.subplots()
+                        ax_roc.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+                        ax_roc.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+                        ax_roc.set_xlim([0.0, 1.0])
+                        ax_roc.set_ylim([0.0, 1.05])
+                        ax_roc.set_xlabel('False Positive Rate')
+                        ax_roc.set_ylabel('True Positive Rate')
+                        ax_roc.set_title('Receiver Operating Characteristic (ROC)')
+                        ax_roc.legend(loc="lower right")
+                        st.pyplot(fig_roc)
+                        
+                        st.subheader("Classification Report")
+                        st.text(class_report)
 
     else:
         st.warning("Please upload the CSV file.")
-
 
 streamlit_logic()
